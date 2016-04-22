73c4f881ab31399e1fdd62fa85090d4207a7b159
曽和修平 <sowashuhei@gmail.com>
2016-04-23 02:12:08 +0900

    分類器のモック作成

--- source/ML/JobClassifier.py
+++ source/ML/JobClassifier.py
@@ -1,18 +1,39 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
 import numpy as np
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-#http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
-#http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.KFold.html#sklearn.cross_validation.KFold
-# http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html#sklearn.decomposition.PCA
+import matplotlib.pyplot as plt
 
+def classify(X_train,X_test,y_train,y_test,isDrawGraph=False):
+	clf = SVC(C=0.5,kernel="linear",degree=2)
+	clf.fit(X_train,y_train)
 
-def classify(X_train,X_test,y_train,y_test):
+	if isDrawGraph:
+		draw(clf,X_train,X_test,y_train,y_test)
+	return clf.score(X_test,y_test)
 
+def draw(clf,X,X_test,y,y_test):
+	h = .02
+	x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+	y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+	xx, yy = np.meshgrid(np.arange(x_min, x_max, h),np.arange(y_min, y_max, h))
+
+	Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+	Z = Z.reshape(xx.shape)
+
+	plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+
+	plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+	plt.xlabel('Sepal length')
+	plt.ylabel('Sepal width')
+	plt.xlim(xx.min(), xx.max())
+	plt.ylim(yy.min(), yy.max())
+	plt.xticks(())
+	plt.yticks(())
+	plt.show()
 
 if __name__ == '__main__':
 
@@ -26,8 +47,18 @@ if __name__ == '__main__':
 	X = data[:,:9]
 	y = data[:,9:10].reshape(1,data_num)[0]
 
+	pca = PCA(n_components=2)
+	pca.fit(X)
+	X = pca.transform(X)
+
 	skf = StratifiedKFold(y, n_folds=10)
+	score = 0
+	count = 0
 	for train_index, test_index in skf:
+		count += 1
 		X_train, X_test = X[train_index], X[test_index]
 		y_train, y_test = y[train_index], y[test_index]
-		classify(X_train,X_test,y_train,y_test)
+		score += classify(X_train,X_test,y_train,y_test,(count==10))
+	score /= 10.0
+	print("平均正答率:"+str(score))
+

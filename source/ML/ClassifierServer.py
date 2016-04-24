b9455979e93a1c13f4ec8eaecacbaa37938945f3
曽和修平 <sowashuhei@gmail.com>
2016-04-24 17:37:28 +0900

    テストデータを渡して結果を受け取るテスト（ノーマライズしてない）

--- source/ML/ClassifierServer.py
+++ source/ML/ClassifierServer.py
@@ -10,11 +10,13 @@ class ClassifierServer(RPCServer):
 		print(data)
 		# scaler = joblib.load("./model/StandardScaler.pkl")
 		# data[:13] = scaler.transform(data[:13])
-		# X = data[:13]
-		# clf = joblib.load("./model/JobClassifier.pkl")
-		# #今は職業に関する予測値のみ返している
-		# r = clf.predict(X)
-		return 1
+		X = data[:13]
+		clf = joblib.load("./model/JobClassifier.pkl")
+		#今は職業に関する予測値のみ返している
+		r = clf.predict(X)
+		print(r)
+
+		return r[0]
 
 server = StreamServer(('127.0.0.1', 6000), ClassifierServer())
 server.serve_forever()
--- source/app.js
+++ source/app.js
@@ -17,7 +17,7 @@ io.sockets.on('connection', function(socket) {
 	socket.on('predict', function(data){
 		console.log("listen");
 		var c = rpc.createClient(6000, '127.0.0.1', function() {
-		  c.invoke('predict', [1,2,3,43], function(err, response) {
+		  c.invoke('predict', [30,1,46.3288,0,-22.411464,-0.028708,-4.91025,20.8182,52.89575,17.55085,73.233,41.6452,66.726], function(err, response) {
 		  	console.log(response);
 		    c.close();
 		  });
